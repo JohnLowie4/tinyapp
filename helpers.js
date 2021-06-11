2ce44c410dd61e61470c208d919e0c73a7072ffe
John <jzlowie@gmail.com>
2021-06-11 03:05:13 -0600

    Added another helper function that checks if user exists

--- helpers.js
+++ helpers.js
@@ -1,3 +1,10 @@
+/**
+ * Searches for all the urls that belongs to a user
+ * 
+ * @param {String} id 
+ * @param {Object} database 
+ * @returns Object
+ */
 const urlsForUser = (id, database) => {
   const ret = {}
   for (const url in database) {
@@ -8,6 +15,11 @@ const urlsForUser = (id, database) => {
   return ret;
 };
 
+/**
+ * Generates a random string of length 6
+ * 
+ * @returns String
+ */
 const generateRandomString = () => {
   const alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
   let ret = "";
@@ -18,7 +30,13 @@ const generateRandomString = () => {
   return ret;
 };
 
-// Checks if user already exists for POST /register
+/**
+ * Searches for a user based on the given email
+ * 
+ * @param {String} userEmail 
+ * @param {Object} database 
+ * @returns Object
+ */
 const getUserByEmail = (userEmail, database) => {
   for (const user in database) {
     if (database[user].email === userEmail) {
@@ -27,8 +45,23 @@ const getUserByEmail = (userEmail, database) => {
   }
 };
 
+/**
+ * Finds if a user already exists in the database
+ * 
+ * @param {String} cookieID 
+ * @param {Object} database 
+ * @returns Object
+ */
+const userExist = (cookieID, database) => {
+  if (database[cookieID]) {
+    return database[cookieID];
+  }
+  return {};
+};
+
 module.exports = {
   urlsForUser,
   generateRandomString,
   getUserByEmail,
+  userExist
 };
